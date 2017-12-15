800eb5a98afea2dc5ec747e1cf2d5ca02b2ce0dc
Hiroki Tei <ohakiki@yahoo.co.jp>
2017-12-15 04:44:21 +0000

    Add User Registration

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,18 +1,23 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in 
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
+  
   def index
-    @tasks = Task.all
+    @user = current_user
+ 
+    @tasks = current_user.tasks
   end
   
   def show
-    @task = Task.find(params[:id])
+
   end
   
   def new 
-    @task = Task.new
+    @task = current_user.tasks.build
   end
   
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
     if @task.save 
       flash[:success] = "正常にタスクが登録されました"
@@ -24,12 +29,10 @@ class TasksController < ApplicationController
   end
   
   def edit
-    @task = Task.find(params[:id])
+
   end
   
   def update
-    @task = Task.find(params[:id])
-    
     if @task.update(task_params)
       flash[:success] = '正常に更新されました'
       redirect_to @task
@@ -40,16 +43,22 @@ class TasksController < ApplicationController
   end
   
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
     
     flash[:success] ="正常に削除されました"
-    redirect_to tasks_url
+    redirect_to root_url
   end
   
   private
   
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user 
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,6 @@
 class Task < ApplicationRecord
+  validates :user_id, presence: true
   validates :status, presence: true, length: {maximum: 10}
+  validates :content, presence: true, length: { maximum: 255 }
+  belongs_to :user
 end
